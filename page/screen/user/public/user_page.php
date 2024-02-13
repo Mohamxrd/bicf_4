595a679e00f1df2f261ab6b3a9787eb7d7d7d6eb
Saliou <macbookpro@192.168.1.10>
2024-02-13 11:09:19 +0000

    ajoiut de page

--- page/screen/user/public/user_page.php
+++ page/screen/user/public/user_page.php
@@ -406,6 +406,16 @@ if ($client = $recupUser->fetch()) {
                                 </a>
                             </li>
 
+                            <li>
+                                <a href="#">
+                                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm11-4a1 1 0 1 0-2 0v4c0 .3.1.5.3.7l3 3a1 1 0 0 0 1.4-1.4L13 11.6V8Z" clip-rule="evenodd" />
+                                    </svg>
+
+                                    <span> Historique </span>
+                                </a>
+                            </li>
+
 
 
                         </ul>
@@ -527,7 +537,7 @@ if ($client = $recupUser->fetch()) {
                             </button>
                         </div>
 
-                        <div class="flex justify-between mb-2">
+                        <div class="flex justify-between">
                             <!-- Dropdown 1 -->
                             <div class="dropdown" style="width: calc(25% - 10px);">
                                 <!-- Dropdown Trigger -->
@@ -581,35 +591,75 @@ if ($client = $recupUser->fetch()) {
 
 
                         <!-- post text-->
-                        <div class="bg-white rounded-xl shadow-sm text-sm font-medium border1 dark:bg-dark2 ">
+                        <div class="bg-white rounded-xl shadow-sm text-sm font-medium border1 dark:bg-dark2 mb-2">
 
 
 
                             <!-- post heading -->
                             <div class="flex gap-3 sm:p-4 p-2.5 text-sm font-medium">
-                                <a href="timeline.html"> <img src="assets/images/avatars/avatar-5.jpg" alt="" class="w-9 h-9 rounded-full"> </a>
                                 <div class="flex-1">
                                     <a href="timeline.html">
-                                        <h4 class="text-black dark:text-white"> John Michael </h4>
+                                        <h4 class="text-lg text-black dark:text-white">Titre de l'annonce </h4>
                                     </a>
-                                    <div class="text-xs text-gray-500 dark:text-white/80"> 2 hours ago</div>
-                                </div>
+                                    <div class="flex items-center text-xs text-gray-500 dark:text-white/80">
+                                        <svg class="w-4 h-4  text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd" d="M12 2a8 8 0 0 1 6.6 12.6l-.1.1-.6.7-5.1 6.2a1 1 0 0 1-1.6 0L6 15.3l-.3-.4-.2-.2v-.2A8 8 0 0 1 11.8 2Zm3 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" clip-rule="evenodd" />
+                                        </svg>
+                                        <span class="ml-1">Abouasso</span>
 
-                                <div class="bg-teal-100 text-teal-600 px-2 py-1.5 rounded-md">Demande</div>
 
-                                <div class="-mr-1">
-                                    <button type="button" class="button__ico w-8 h-8" aria-haspopup="true" aria-expanded="false"> <ion-icon class="text-xl md hydrated" name="ellipsis-horizontal" role="img" aria-label="ellipsis horizontal"></ion-icon> </button>
-                                    <div class="w-[245px] uk-dropdown" uk-dropdown="pos: bottom-right; animation: uk-animation-scale-up uk-transform-origin-top-right; animate-out: true; mode: click">
-                                        <nav>
-                                            <a href="#"> <ion-icon class="text-xl shrink-0 md hydrated" name="bookmark-outline" role="img" aria-label="bookmark outline"></ion-icon> Add to favorites </a>
-                                            <a href="#"> <ion-icon class="text-xl shrink-0 md hydrated" name="notifications-off-outline" role="img" aria-label="notifications off outline"></ion-icon> Mute Notification </a>
-                                            <a href="#"> <ion-icon class="text-xl shrink-0 md hydrated" name="flag-outline" role="img" aria-label="flag outline"></ion-icon> Report this post </a>
-                                            <a href="#"> <ion-icon class="text-xl shrink-0 md hydrated" name="share-outline" role="img" aria-label="share outline"></ion-icon> Share your profile </a>
-                                            <hr>
-                                            <a href="#" class="text-red-400 hover:!bg-red-50 dark:hover:!bg-red-500/50"> <ion-icon class="text-xl shrink-0 md hydrated" name="stop-circle-outline" role="img" aria-label="stop circle outline"></ion-icon> Unfollow </a>
-                                        </nav>
                                     </div>
                                 </div>
+                                <div class="flex bg-teal-100 text-teal-600 p-3 rounded-md items-center">Demande</div>
+                            </div>
+
+
+                            <div class="sm:px-4 p-2.5 pt-0">
+                                <p class="font-normal"> Photography is the art of capturing light with a camera. It can be used to create images that tell stories, express emotions, or document reality. it can be fun, challenging, or rewarding. It can also be a hobby, a profession, or a passion. 📷 </p>
+                            </div>
+
+                            <!-- post icons -->
+
+
+                            <!-- comments -->
+
+                            <!-- add comment -->
+                            <div class="flex justify-between items-center  dark:text-white/80 p-4 w-full">
+                                <button class="text-white p-2 bg-blue-500 rounded-md">Répondre à la demande</button>
+                                <div class="text-xs text-gray-500">2 hours ago</div>
+                            </div>
+
+
+
+
+                        </div>
+
+                        <div class="bg-white rounded-xl shadow-sm text-sm font-medium border1 dark:bg-dark2 mb-2">
+
+
+
+                            <!-- post heading -->
+                            <div class="flex gap-3 sm:p-4 p-2.5 text-sm font-medium">
+
+                                <div class="flex-1">
+                                    <a href="timeline.html">
+                                        <h4 class="text-lg text-black dark:text-white">Titre de l'annonce </h4>
+                                    </a>
+                                    <div class="flex items-center text-xs text-gray-500 dark:text-white/80">
+                                        <svg class="w-4 h-4  text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd" d="M12 2a8 8 0 0 1 6.6 12.6l-.1.1-.6.7-5.1 6.2a1 1 0 0 1-1.6 0L6 15.3l-.3-.4-.2-.2v-.2A8 8 0 0 1 11.8 2Zm3 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" clip-rule="evenodd" />
+                                        </svg>
+                                        <span class="ml-1">San pedro</span>
+
+
+                                    </div>
+
+                                </div>
+
+                                <div class="flex bg-orange-100 text-orange-600 p-3 rounded-md items-center">Offre</div>
+
+
+
                             </div>
 
                             <div class="sm:px-4 p-2.5 pt-0">
@@ -622,30 +672,13 @@ if ($client = $recupUser->fetch()) {
                             <!-- comments -->
 
                             <!-- add comment -->
-                            <div class="sm:px-4 sm:py-3 p-2.5 border-t border-gray-100 flex items-center gap-1 dark:border-slate-700/40">
-
-                                <img src="assets/images/avatars/avatar-7.jpg" alt="" class="w-6 h-6 rounded-full">
-
-                                <div class="flex-1 relative overflow-hidden h-10">
-                                    <textarea placeholder="Add Comment...." rows="1" class="w-full resize-none !bg-transparent px-4 py-2 focus:!border-transparent focus:!ring-transparent" aria-haspopup="true" aria-expanded="false"></textarea>
-
-                                    <div class="!top-2 pr-2 uk-drop" uk-drop="pos: bottom-right; mode: click">
-                                        <div class="flex items-center gap-2" uk-scrollspy="target: > svg; cls: uk-animation-slide-right-small; delay: 100 ;repeat: true">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 fill-sky-600" style="opacity: 0;">
-                                                <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd"></path>
-                                            </svg>
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 fill-pink-600" style="opacity: 0;">
-                                                <path d="M3.25 4A2.25 2.25 0 001 6.25v7.5A2.25 2.25 0 003.25 16h7.5A2.25 2.25 0 0013 13.75v-7.5A2.25 2.25 0 0010.75 4h-7.5zM19 4.75a.75.75 0 00-1.28-.53l-3 3a.75.75 0 00-.22.53v4.5c0 .199.079.39.22.53l3 3a.75.75 0 001.28-.53V4.75z"></path>
-                                            </svg>
-                                        </div>
-                                    </div>
-
-
-                                </div>
-
-
-                                <button type="submit" class="text-sm rounded-full py-1.5 px-3.5 bg-secondery"> Replay</button>
+                            <div class="flex justify-between items-center  dark:text-white/80 p-4 w-full">
+                                <button class="text-white p-2 bg-blue-500 rounded-md">Envoyé une demande</button>
+                                <div class="text-xs text-gray-500">2 hours ago</div>
                             </div>
+
+
+
 
                         </div>
 
