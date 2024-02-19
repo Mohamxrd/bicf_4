2d20baf904e05dc64e3b4e8c5989efca114062bb
Saliou <macbookpro@192.168.1.32>
2024-02-19 15:57:15 +0000

    ajout

--- page/screen/user/public/addprod.php
+++ page/screen/user/public/addprod.php
@@ -37,6 +37,9 @@ if ($client = $recupUser->fetch()) {
     echo "Erreur: Utilisateur non trouvé dans la base de données.";
     exit();
 }
+
+
+
 ?>
 
 
@@ -227,113 +230,7 @@ if ($client = $recupUser->fetch()) {
                                 <div class="text-sm h-[400px] w-full overflow-y-auto pr-2">
 
                                     <!-- contents list -->
-                                    <div class="pl-2 p-1 text-sm font-normal dark:text-white">
-
-                                        <a href="#"
-                                            class="relative flex items-center gap-3 p-2 duration-200 rounded-xl pr-10 hover:bg-secondery dark:hover:bg-white/10 bg-teal-500/5">
-                                            <div class="relative w-12 h-12 shrink-0"> <img
-                                                    src="assets/images/avatars/avatar-3.jpg" alt=""
-                                                    class="object-cover w-full h-full rounded-full"></div>
-                                            <div class="flex-1 ">
-                                                <p> <b class="font-bold mr-1"> Alexa Gray</b> started following you.
-                                                    Welcome him to your profile. 👋 </p>
-                                                <div class="text-xs text-gray-500 mt-1.5 dark:text-white/80"> 4 hours
-                                                    ago </div>
-                                                <div
-                                                    class="w-2.5 h-2.5 bg-teal-600 rounded-full absolute right-3 top-5">
-                                                </div>
-                                            </div>
-                                        </a>
-                                        <a href="#"
-                                            class="relative flex items-center gap-3 p-2 duration-200 rounded-xl pr-10 hover:bg-secondery dark:hover:bg-white/10">
-                                            <div class="relative w-12 h-12 shrink-0"> <img
-                                                    src="assets/images/avatars/avatar-7.jpg" alt=""
-                                                    class="object-cover w-full h-full rounded-full"></div>
-                                            <div class="flex-1 ">
-                                                <p> <b class="font-bold mr-1">Jesse Steeve</b> mentioned you in a story.
-                                                    Check it out and reply. 📣 </p>
-                                                <div class="text-xs text-gray-500 mt-1.5 dark:text-white/80"> 8 hours
-                                                    ago </div>
-                                            </div>
-                                        </a>
-                                        <a href="#"
-                                            class="relative flex items-center gap-3 p-2 duration-200 rounded-xl pr-10 hover:bg-secondery dark:hover:bg-white/10">
-                                            <div class="relative w-12 h-12 shrink-0"> <img
-                                                    src="assets/images/avatars/avatar-6.jpg" alt=""
-                                                    class="object-cover w-full h-full rounded-full"></div>
-                                            <div class="flex-1 ">
-                                                <p> <b class="font-bold mr-1"> Alexa stella</b> commented on your photo
-                                                    “Wow, stunning shot!” 💬 </p>
-                                                <div class="text-xs text-gray-500 mt-1.5 dark:text-white/80"> 8 hours
-                                                    ago </div>
-                                            </div>
-                                        </a>
-                                        <a href="#"
-                                            class="relative flex items-center gap-3 p-2 duration-200 rounded-xl pr-10 hover:bg-secondery dark:hover:bg-white/10">
-                                            <div class="relative w-12 h-12 shrink-0"> <img
-                                                    src="assets/images/avatars/avatar-2.jpg" alt=""
-                                                    class="object-cover w-full h-full rounded-full"></div>
-                                            <div class="flex-1 ">
-                                                <p> <b class="font-bold mr-1"> John Michael</b> who you might know, is
-                                                    on socialite.</p>
-                                                <div class="text-xs text-gray-500 mt-1.5 dark:text-white/80"> 2 hours
-                                                    ago </div>
-                                            </div>
-                                            <button type="button" class="button text-white bg-primary">fallow</button>
-                                        </a>
-                                        <a href="#"
-                                            class="relative flex items-center gap-3 p-2 duration-200 rounded-xl pr-10 hover:bg-secondery dark:hover:bg-white/10 bg-teal-500/5">
-                                            <div class="relative w-12 h-12 shrink-0"> <img
-                                                    src="assets/images/avatars/avatar-3.jpg" alt=""
-                                                    class="object-cover w-full h-full rounded-full"></div>
-                                            <div class="flex-1 ">
-                                                <p> <b class="font-bold mr-1"> Sarah Gray</b> sent you a message. He
-                                                    wants to chat with you. 💖 </p>
-                                                <div class="text-xs text-gray-500 mt-1.5 dark:text-white/80"> 4 hours
-                                                    ago </div>
-                                                <div
-                                                    class="w-2.5 h-2.5 bg-teal-600 rounded-full absolute right-3 top-5">
-                                                </div>
-                                            </div>
-                                        </a>
-                                        <a href="#"
-                                            class="relative flex items-center gap-3 p-2 duration-200 rounded-xl pr-10 hover:bg-secondery dark:hover:bg-white/10">
-                                            <div class="relative w-12 h-12 shrink-0"> <img
-                                                    src="assets/images/avatars/avatar-4.jpg" alt=""
-                                                    class="object-cover w-full h-full rounded-full"></div>
-                                            <div class="flex-1 ">
-                                                <p> <b class="font-bold mr-1"> Jesse Steeve</b> sarah tagged you <br> in
-                                                    a photo of your birthday party. 📸 </p>
-                                                <div class="text-xs text-gray-500 mt-1.5 dark:text-white/80"> 8 hours
-                                                    ago </div>
-                                            </div>
-                                        </a>
-                                        <a href="#"
-                                            class="relative flex items-center gap-3 p-2 duration-200 rounded-xl pr-10 hover:bg-secondery dark:hover:bg-white/10">
-                                            <div class="relative w-12 h-12 shrink-0"> <img
-                                                    src="assets/images/avatars/avatar-2.jpg" alt=""
-                                                    class="object-cover w-full h-full rounded-full"></div>
-                                            <div class="flex-1 ">
-                                                <p> <b class="font-bold mr-1"> Lewis Lewis</b> mentioned you in a story.
-                                                    Check it out and reply. 📣 </p>
-                                                <div class="text-xs text-gray-500 mt-1.5 dark:text-white/80"> 8 hours
-                                                    ago </div>
-                                            </div>
-                                        </a>
-                                        <a href="#"
-                                            class="relative flex items-center gap-3 p-2 duration-200 rounded-xl pr-10 hover:bg-secondery dark:hover:bg-white/10">
-                                            <div class="relative w-12 h-12 shrink-0"> <img
-                                                    src="assets/images/avatars/avatar-7.jpg" alt=""
-                                                    class="object-cover w-full h-full rounded-full"></div>
-                                            <div class="flex-1 ">
-                                                <p> <b class="font-bold mr-1"> Martin Gray</b> liked your photo of the
-                                                    Eiffel Tower. 😍 </p>
-                                                <div class="text-xs text-gray-500 mt-1.5 dark:text-white/80"> 8 hours
-                                                    ago </div>
-                                            </div>
-                                        </a>
-
-                                    </div>
+                                    
 
                                 </div>
 
